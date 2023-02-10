34b09d027041699d15318da3718738f1bb6d0a62
HellenTamara <hellen.tamara@outlook.com>
2023-02-10 16:43:06 +0900

    10/02/2023

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+// Cube
+let x = 0
+let y = 20
+let z = 0
+let bool = true
+let interval;
+
+const cube = document.querySelector('.cube')
+
+document.querySelector('.top-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.bottom-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.left-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.right-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.top-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg)`
+})
+
+document.querySelector('.bottom-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg)`
+})
+
+
+const playPause = () => {
+    if(bool){
+        interval = setInterval(() => {
+            cube.style.transform = `rotateY(${y++}deg) rotateX(${x}deg) rotateZ(${z}deg)`
+        }, 100)
+    } else {
+        clearInterval(interval)
+    }
+   
+}
+
+playPause()
+
+document.querySelector('.controls').addEventListener('mouseover', () => {
+    bool = false
+    playPause()
+})
+
+document.querySelector('.controls').addEventListener('mouseout', () => {
+    bool = true
+    playPause()
+})
+// End of Cube
+
 // Slideshow
 const slideshowDivs = () => {
     for(let i = 1; i <= 5; i++) {
@@ -31,7 +89,7 @@ const slideshow = () => {
             divs[0].classList.add('change')
             a = 1
         }
-    }, 5000)
+    }, 20000)
 }
 
 slideshow()
