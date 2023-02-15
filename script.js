77f35edc9bd3f95c90e2b494e997b75e3a592753
HellenTamara <hellen.tamara@outlook.com>
2023-02-15 17:14:26 +0900

    15/02/2023

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Common JS
-document.querySelectorAll('.wacth-control, ')
+document.querySelectorAll('.watch-control, .controls a, .iphone-btn').forEach(control => {
+    control.addEventListener('click', e => {
+        e.preventDefault()
+    })
+})
 // End of Common JS
 
 // Cube
@@ -117,4 +121,55 @@ const watchTopControl = document.querySelector('.watch-top-control')
 const watchLeftControl = document.querySelector('.watch-left-control')
 const watchRightControl = document.querySelector('.watch-right-control')
 const watchBottompControl = document.querySelector('.watch-bottom-control')
+
+
+let axisY = 0
+let axisX = 0
+
+const hideControl = () => {
+    if(axisY === -280){
+        watchTopControl.classList.add('hideControl')
+    } else {
+        watchTopControl.classList.remove('hideControl')
+    }
+
+    if(axisY === 280){
+        watchBottompControl.classList.add('hideControl')
+    } else {
+        watchBottompControl.classList.remove('hideControl')
+    }
+
+    if(axisX === 280){
+        watchRightControl.classList.add('hideControl')
+    } else {
+        watchRightControl.classList.remove('hideControl')
+    }
+
+    if(axisX === -280){
+        watchLeftControl.classList.add('hideControl')
+    } else {
+        watchLeftControl.classList.remove('hideControl')
+    }
+}
+
+watchTopControl.addEventListener('click', () => {
+    watchCases.style.marginTop = `${axisY -= 70}rem`
+    hideControl()
+})
+
+
+watchBottompControl.addEventListener('click', () => {
+    watchCases.style.marginTop = `${axisY += 70}rem`
+    hideControl()
+})
+
+watchRightControl.addEventListener('click', () => {
+    watchBands.style.marginRight = `${axisX += 70}rem`
+    hideControl()
+})
+
+watchLeftControl.addEventListener('click', () => {
+    watchBands.style.marginRight = `${axisX -= 70}rem`
+    hideControl()
+})
 // End of Section 4
